a46901c2b6b547e9bbdc8365b8d6cc683ba85555
Bernard Historillo <bernard.historillo@bicol-u.edu.ph>
2021-11-11 12:34:09 +0800

    Fix token owners in favorited tokens page

--- js/script.js
+++ js/script.js
@@ -59,8 +59,8 @@ let hasMarketplacePolygonContract = false;
 let initializeEnvVariables = () => {
     let currentURL = window.location.href;
 
-    if(currentURL.includes("ownly.market")) {
-    // if(true) {
+    // if(currentURL.includes("ownly.market")) {
+    if(true) {
         titansContractAddress = "0x804efc52BFa9B85A86219c191d51147566f3B327";
         titansContractAbi = [{"inputs":[{"internalType":"address","name":"admin_","type":"address"},{"internalType":"string","name":"name_","type":"string"},{"internalType":"string","name":"symbol_","type":"string"}],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"approved","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"operator","type":"address"},{"indexed":false,"internalType":"bool","name":"approved","type":"bool"}],"name":"ApprovalForAll","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"address","name":"account","type":"address"}],"name":"Paused","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"address","name":"account","type":"address"}],"name":"Unpaused","type":"event"},{"inputs":[],"name":"admin","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"approve","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"string","name":"tokenURI","type":"string"}],"name":"createToken","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"getApproved","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"operator","type":"address"}],"name":"isApprovedForAll","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"ownerOf","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"paused","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"},{"internalType":"bytes","name":"_data","type":"bytes"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"operator","type":"address"},{"internalType":"bool","name":"approved","type":"bool"}],"name":"setApprovalForAll","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"setPause","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes4","name":"interfaceId","type":"bytes4"}],"name":"supportsInterface","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"index","type":"uint256"}],"name":"tokenByIndex","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"uint256","name":"index","type":"uint256"}],"name":"tokenOfOwnerByIndex","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"tokenURI","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"transferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"unsetPause","outputs":[],"stateMutability":"nonpayable","type":"function"}];
 
@@ -772,27 +772,27 @@ let displayFavoritedTokens = async function(profile, page) {
             if(parseInt(metadata[i].chain_id) === chainIDBsc) {
                 marketplaceBinanceContract.methods.fetchMarketItem(metadata[i].contract_address, metadata[i].id).call()
                     .then(async function(marketItem) {
-                        await formatTokenCards(null, "favorites", metadata[i].id, marketItem, metadata[i], profile, metadata[i].contract_address, "bsc");
+                        await formatTokenCards(null, "favorites", metadata[i].id, marketItem, metadata[i], metadata[i].to, metadata[i].contract_address, "bsc");
                     });
             } else if(parseInt(metadata[i].chain_id) === chainIDEth) {
                 if(hasMarketplaceEthereumContract) {
                     marketplaceEthereumContract.methods.fetchMarketItem(ownedTokens[i].contract_address, ownedTokens[i].id).call()
                         .then(async function(marketItem) {
-                            await formatTokenCards(null, "favorites", metadata[i].id, marketItem, metadata[i], profile, metadata[i].contract_address, "eth");
+                            await formatTokenCards(null, "favorites", metadata[i].id, marketItem, metadata[i], metadata[i].to, metadata[i].contract_address, "eth");
                         });
                 } else {
                     let marketItem = false;
-                    await formatTokenCards(null, "owned", metadata[i].id, marketItem, metadata[i], profile, metadata[i].contract_address, "eth");
+                    await formatTokenCards(null, "owned", metadata[i].id, marketItem, metadata[i], metadata[i].to, metadata[i].contract_address, "eth");
                 }
             } else if(parseInt(metadata[i].chain_id) === chainIDMatic) {
                 if(hasMarketplacePolygonContract) {
                     marketplacePolygonContract.methods.fetchMarketItem(ownedTokens[i].contract_address, ownedTokens[i].id).call()
                         .then(async function(marketItem) {
-                            await formatTokenCards(null, "favorites", metadata[i].id, marketItem, metadata[i], profile, metadata[i].contract_address, "eth");
+                            await formatTokenCards(null, "favorites", metadata[i].id, marketItem, metadata[i], metadata[i].to, metadata[i].contract_address, "eth");
                         });
                 } else {
                     let marketItem = false;
-                    await formatTokenCards(null, "favorites", metadata[i].id, marketItem, metadata[i], profile, metadata[i].contract_address, "eth");
+                    await formatTokenCards(null, "favorites", metadata[i].id, marketItem, metadata[i], metadata[i].to, metadata[i].contract_address, "eth");
                 }
             }
         }
@@ -828,6 +828,8 @@ let formatTokenCards = async function(excludedToken, type, i, marketItem, metada
         explorerName = "PolygonScan";
         blockchainExplorer = blockchainExplorerMatic;
     }
+
+    console.log(owner);
 
     let col_xl = "col-xl-4";
     let padding_top = "100%";
